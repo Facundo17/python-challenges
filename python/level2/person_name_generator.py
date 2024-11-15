bc0ae2c2faf5614c2da70749ae978072834bceff
Facundo Nahuel Saucedo <fnahuel@getontop.com>
2024-11-15 11:10:15 -0300

    Two new challenges completed

---
+++ python/level2/person_name_generator.py
@@ -0,0 +1,52 @@
+import random
+# TODO: mejorar la entrada de usuario, no se está validando o manjando excepciones
+
+# usando el name.txt
+file = '../names.txt'
+
+def read_file() -> list:
+    names = []
+    with open(file, "r") as lines:
+        linesText = lines.readlines()
+        names = list(linesText)
+    return names
+
+def write_file(name):
+    with open(file, "at") as lines:
+        lines.write(f"\n{name}")
+    
+def select_random() -> str:
+    names = read_file()
+    return random.choice(names) # la función random devuelve una función aleatoria en una secuencia (list, tuple o string)
+
+def welcome_message() -> str:
+    print("""Please select an option:
+1. Show the total of names
+2. Add a new name
+3. Select random name
+4. Exit""")
+
+def choice(num):
+    match(num):
+        case 1:
+            print("\nTotal number of names: ", len(read_file()), "\n")
+        case 2:
+            newName = input("Enter the new name to add: ")
+            
+            write_file(newName)
+            
+            print(f"{newName} has been added to the file.")
+        case 3:
+            print(f"\nRandom name selected: {select_random()}")
+        case 4:
+            print("Exit")
+
+while True:
+    
+    welcome_message()
+    userInput = input("Enter your choice (1-4): ")
+    
+    choice(int(userInput))
+    
+    if int(userInput) == 4:
+        break
